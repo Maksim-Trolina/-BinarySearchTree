842310485b880dde689dd1b7ebf364272db0dabb
Maksim <maks.panasenko01@bk.ru>
2020-12-25 02:33:06 +0300

    Add test work

--- BinarySeachTree/BinarySeachTree/Program.cs
+++ BinarySeachTree/BinarySeachTree/Program.cs
@@ -6,7 +6,33 @@ namespace BinarySeachTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinarySeachTree<string> binarySeachTree = new BinarySeachTree<string>();
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int key = Convert.ToInt32(Console.ReadLine());
+                binarySeachTree.Insert(key,Console.ReadLine());
+            }
+
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                int key = Convert.ToInt32(Console.ReadLine());
+                
+                binarySeachTree.Remove(key);
+            }
+
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < d; i++)
+            {
+                int key = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine(binarySeachTree.Contains(key));
+            }
         }
     }
 }
